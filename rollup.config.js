7787aeb73ff960c2e65d466ce43c7e302a2a0020
liaozhai <laocan@yandex.ru>
2020-05-12 13:12:25 +0300

    Updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,18 @@
-import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 import css from 'rollup-plugin-css-porter';
 
 export default [
     {
         input: 'example/App.js',
         output: { 
-            file: pkg.browser,
+            file: 'public/main.js',
             format: 'iife',
             sourcemap: true,
             name: 'Example'
         },
         plugins: [                        
-            json(),
             resolve(),            
             commonjs(),
             css({dest: 'public/main.css', minified: false}),            
@@ -29,19 +26,18 @@ export default [
     {
         input: 'src/Sidebar.js',
         output: { 
-            file: pkg.module,
+            file: 'dist/scanex-sidebar.js',
             format: 'cjs',
             sourcemap: true
         },
         plugins: [        
-            json(),
             resolve(),
             commonjs(),
             css({dest: 'dist/scanex-sidebar.css', minified: false}),
             babel({                
                 extensions: ['.js', '.mjs'],
                 exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'],
-                include: ['index.js', 'src/**']
+                include: ['src/**']
             }),
         ],
     },   
